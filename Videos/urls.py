0da8c479c3b95727adf565b36d9ea406f9b3a656
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-10 22:45:40 +0300

    auth views and signup template

--- Videos/urls.py
+++ Videos/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from halls import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    # Auth
+    path('signup', views.SignUp.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
